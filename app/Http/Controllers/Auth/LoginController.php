55250881cd3cbeacff132a894686768eac6df5e2
naorone <naorone@gmail.com>
2017-11-06 13:48:10 +0100

    verificado final

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
@@ -39,9 +40,30 @@ class LoginController extends Controller
     }
 
 
-    protected function credentials($request)
+    protected function credentials(Request $request)
     {
         return ['email' => $request->{$this->username()}, 'password' => $request->password, 'verified' => 1];
+    }
+
+    public function sendFailedLoginResponse(Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+
+        // Load user from database
+        $user =User::where($this->username(), $request->{$this->username()})->first();
+
+        // Check if user was successfully loaded, that the password matches
+        // and active is not 1. If so, override the default error message.
+        if ($user && \Hash::check($request->password, $user->password) && $user->confirmed != 1) {
+            $errors = [$this->username() => trans('auth.verification')];
+        }
+
+        /*if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }*/
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
     }
 
 
--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'These credentials do not match our records.',
+    'verification' => 'Email sin verificar',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 
